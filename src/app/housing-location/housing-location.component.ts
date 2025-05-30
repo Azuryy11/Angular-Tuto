2f2ee3f79baa142f35fe4eb3cfa2afb77d0132cb
Azuryy11 <sebastien.lemeu@gmail.com>
2025-05-30 15:51:57 +0200

    Routes Details

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,26 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HousingLocation} from '../housinglocation';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   template: `
     <section class="listing">
       <img
-        *ngIf = "housingLocation.photo" 
+        *ngIf = "housingLocation().photo" 
         class="listing-photo"
-        [src]="housingLocation.photo"
-        alt="Exterior photo of {{ housingLocation.name }}"
+        [src]="housingLocation().photo"
+        alt="Exterior photo of {{ housingLocation().name }}"
         crossorigin
       />
-      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
-      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <h2 class="listing-heading">{{ housingLocation().name }}</h2>
+      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+      <a [routerLink]="['/details', housingLocation().id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css'],
 })
 
 export class HousingLocationComponent {
-  @Input() housingLocation!: HousingLocation;
+  housingLocation = input.required<HousingLocation>();
 }
